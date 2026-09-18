f7275aab15abf18bea46f6714a7b6af211638867
Djongaledi <125315144+Djongaledi@users.noreply.github.com>
2026-09-18 02:29:23 +0200

    Update app.py

--- app.py
+++ app.py
@@ -251,7 +251,8 @@ elif choix == "2. Scanner de Présence":
             if succes_p:
                 st.success(message_p)
             else:
-            if "déjà pointé" in message_p: st.warning(message_p):
+            if "déjà pointé" in message_p: 
+                st.warning(message_p)
             else:
                 st.error(message_p)
             
